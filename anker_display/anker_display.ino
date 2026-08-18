5d00cd01024250194dd7b1bcaed2d02cf131b398
det <det@dets-Mac-mini-381.local>
2026-08-18 14:57:59 +0200

    1.29.0: hoehere Version zum Pruefen der Update-Anzeige
    Inhaltlich identisch mit 1.28.0. Zweck: ein Geraet auf 1.28.0 muss
    damit den gelben Punkt zeigen.

--- anker_display/anker_display.ino
+++ anker_display/anker_display.ino
@@ -39,7 +39,7 @@
   SPDX-License-Identifier: PolyForm-Noncommercial-1.0.0
   Copyright (c) 2026 Detlev Euskirchen
 */
-#define FW_VERSION "1.28.0"
+#define FW_VERSION "1.29.0"
 
 // Ausfuehrliche Ausgaben im seriellen Monitor.
 //   1 = jede MQTT-Nachricht wird protokolliert (zum Mitlesen und Decodieren)
